a61beca7b0e2a1ba925c1bec8404998e3b9d4047
Ashley Davison <ashley.e.davison@gmail.com>
2016-02-11 18:53:24 -0500

    feat: Add second test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('defined a url that is not empty for each feed', function() {
+           allFeeds.forEach(function(feed){
+             expect(feed.url).toBeDefined();
+             expect(feed.url.length).not.toBe(0);
+           });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
