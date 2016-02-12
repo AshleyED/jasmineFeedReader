091cd41857b090fb4479d306007dcdfa2528a76d
Ashley Davison <ashley.e.davison@gmail.com>
2016-02-11 21:48:02 -0500

    feat: Add third test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -52,7 +52,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe('The menu', function(){
+    describe('The menu', function() {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
@@ -75,7 +75,10 @@ $(function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
           });
     });
+
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -83,7 +86,14 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-
+         beforeEach(function (done) {
+            loadFeed (0, done);
+          //  done();
+         });
+         it('should ensure the loadFeed calls at least one entry', function () {
+            expect($('.entry, .feed').length).toBeGreaterThan(0);
+         });
+      });
     /* TODO: Write a new test suite named "New Feed Selection"
 
         /* TODO: Write a test that ensures when a new feed is loaded
